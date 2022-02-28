32efe2c465cdb9ea84c1cade0142803e08d73a16
Christian <christian.jhardy.steinmann@gmail.com>
2022-02-28 21:45:53 +0200

    Fixed import route

--- contracts/DecentralBankContract.sol
+++ contracts/DecentralBankContract.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
-import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 contract DecentralBankContract is ERC20 {
 
